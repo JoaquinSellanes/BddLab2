480a76d57facee2092a5441f781df887a296838b
Joaquin Sellanes <joaquin.sellanes@estudiantes.utec.edu.uy>
2023-06-10 05:54:09 -0300

    venta articulo y venta entrada

--- SP&TR.sql
+++ SP&TR.sql
@@ -8,15 +8,65 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
-
 CREATE TRIGGER TR_Entradas
 AFTER INSERT ON ventaentrada
 FOR EACH ROW
 EXECUTE FUNCTION SP_Entradas();
 
-INSERT INTO venta (fecha, valor) VALUES ('2023-05-11', 0);
-INSERT INTO ventaEntrada (idVenta, esMenor, diaDeLaSemana) VALUES (39, true, 'jueves');
+---------------------------------------------------------------------------------------
+
+CREATE FUNCTION SP_VArticulos() RETURNS TRIGGER AS $$
+BEGIN
+	UPDATE stock
+	SET stock = stock - NEW.cantidad
+	WHERE idarticulo = NEW.idarticulo;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER TR_VArticulos
+AFTER INSERT ON ventaarticulo
+FOR EACH ROW
+EXECUTE FUNCTION SP_VArticulos();
 
 ---------------------------------------------------------------------------------------
 
+CREATE OR REPLACE FUNCTION SP_ExisteAriculo(newid integer) RETURNS boolean AS $$
+DECLARE
+    contador integer;
+BEGIN
+    SELECT COUNT(*) INTO contador
+    FROM articulo
+    WHERE id = newid;
+
+    RETURN contador > 0;
+END;
+$$ LANGUAGE plpgsql;
+
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION SP_ExisteStock(newid integer, newcant integer) RETURNS boolean AS $$
+DECLARE
+    foo integer;
+BEGIN
+    SELECT stock INTO foo
+    FROM stock
+	WHERE idarticulo = newid;
+
+    RETURN newcant <= foo;
+END;
+$$ LANGUAGE plpgsql;
+
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION SP_ValorArticulo(newid integer) RETURNS integer AS $$
+DECLARE
+	foo integer;
+BEGIN
+	SELECT valorventa INTO foo
+	FROM articulo
+	WHERE id = newid;
+
+	RETURN foo;
+END;
+$$ LANGUAGE plpgsql;
